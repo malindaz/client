ad3ae5c09a08fbe61fee83346d9190b70479a31e
Malinda Umesh <malindaumesh093@gmail.com>
2023-12-20 21:39:59 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,68 +1,22 @@
-import React, { Component } from 'react' 
-import axios from 'axios' ;
+import React, { Component} from "react";
+import{ BrowserRouter,Router } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import EditPost from "./components/EditPost";
+import PostDetails from "./components/PostDetails";
 
-export default class App extends Component{
-  constructor(props){
-    super(props);
 
-    this.state={ 
-      posts :[]
-    };
-
+export default class App extends Component {
+  render(){
+    return(
+      <div className = "container">
+        <NavBar/>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/add" component={CreatePost}></Route>
+        <Route path="/edit/:id" component={EditPost}></Route>
+        <Route path="/post/:id" component={PostDetails}></Route>
+        
+      </div>
+    )
   }
-
-  componentDidMount(){
-    this.retrievePosts();
-  }
-
-retrievePosts(){
-  axios.get("http://localhost:8001/posts").then(res=>{
-    if (res.data.success){
-      this.setState({
-        posts:res.data.existingPosts
-      });
-      console.log(this.state.posts)
-    }
-  });
 }
-
-render(){
-  return(
-    <div className="container">
-      <p><h2>All posts</h2></p>
-      <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Topic</th>
-              <th scope="col">Description</th>
-              <th scope="col">Post Category</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {this.state.posts.map((posts,index)=>(
-              <tr>
-                <th scope="row">{index+1}</th>
-                <td>{posts.topic}</td>
-                <td>{posts.Description}</td>
-                <td>{posts.postCategory}</td>
-                <td>
-                  <a className="btn btn-warning" href="#">
-                    <i className="fas fa-edit"></i>&nbsp;
-                    Edit
-                  </a>
-                  <a className="btn btn-danger" href="#">
-                  <i className="fas trash-alt"></i>&nbsp;
-                    Delete
-                  </a>
-                </td>
-
-              </tr>
-            ))}
-          </tbody>
-        </table>
-    </div>
-  )
-  }}
---
+++ src/components/CreatePost.js
@@ -0,0 +1,98 @@
+import React, { Component} from "react";
+import axios from "axios";
+
+
+export default class CreatePost extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            topic:"",
+            description:"",
+            postCategory:""
+        }
+    }
+
+    handleInputChange = (e) =>{
+        const {name,value} = e.target;
+        this.setState({
+            ...this.state,
+            [name]:value
+        })
+    }
+
+    onSubmit = (e) =>{
+        e.preventDefault();
+        const {topic,description,postCategory}=this.state;
+        const data ={
+            topic:topic,
+            description:description,
+            postCategory:postCategory
+
+        }
+        console.log(data)
+
+        axios.post("/post/save",data).then((res)=>{
+            if(res.data.success){
+                this.setState(
+                    {
+                        topic:"",
+                        description:"",
+                        postCategory:""  
+                    }
+                )
+            }
+        }
+    )
+
+    }
+  render(){
+    return(
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Create new post</h1>
+        <form className="need-validation" noValidate>
+            <div className="form-group" style={{marginBottom:'15px'}}>
+                <lable style = {{marginBottom:'5px'}}>Topic</lable>
+                 <input type = "text"
+                 className="form-control"
+                 name="topic"
+                 placeholder="Enter Topic"
+                 value={this.state.topic}
+                 onChange={this.handleInputChange}/>
+            </div>
+
+
+            <div className="form-group" style={{marginBottom:'15px'}}>
+                <lable style = {{marginBottom:'5px'}}>Description</lable>
+                 <input type = "text"
+                 className="form-control"
+                 name="description"
+                 placeholder="Enter description"
+                 value={this.state.description}
+                 onChange={this.handleInputChange}/>
+            </div>
+
+
+            <div className="form-group" style={{marginBottom:'15px'}}>
+                <lable style = {{marginBottom:'5px'}}>post Category</lable>
+                 <input type = "text"
+                 className="form-control"
+                 name="postCategory"
+                 placeholder="Enter Post Category"
+                 value={this.state.postCategory}
+                 onChange={this.handleInputChange}/>
+            </div>
+
+            <button className="btn btn-success" 
+            type="submit"
+            style={{marginTop:'15px'}}
+            onClick={this.onSubmit}>
+                Save
+            </button>
+            
+        </form>
+       
+      </div>
+    )
+  }
+}
